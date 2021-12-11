8ad74991816d71ce0df4e30da6e6a82c8db7c0ce
Rafael da Cunha Theodoro <rafaeldacunhatheodoro@gmail.com>
2021-12-11 19:12:10 -0300

    hardwork

--- acao.js
+++ acao.js
@@ -1,3 +1,6 @@
+
+//validações 
+
 let nome = document.querySelector('#nome')
 let labelNome = document.querySelector('#labelNome')
 let validNome = false
@@ -6,30 +9,21 @@ let email = document.querySelector('#email')
 let labelEmail = document.querySelector('#labelEmail')
 let validEmail = false
 
-let confirmEmail = document.querySelector('#confirmEmail')
-let labelConfirmEmail = document.querySelector('#labelEmail')
-let validConfirmEmail = false
-
-
 let cpf = document.querySelector('#cpf')
 let labelCpf = document.querySelector('#labelCpf')
 let validCpf = false
-
 
 let telefone = document.querySelector('#telefone')
 let labelTelefone = document.querySelector('#labelTelefone')
 let validTelefone = false
 
-
 let msgError = document.querySelector('#msgError')
 let msgSuccess = document.querySelector('#msgSuccess')
 
-
-
-
 //campo nome
 nome.addEventListener('keyup', () => {
-  if(nome.value.length <= "2" || nome.value.length == ""){
+  
+  if (nome.value.length <= "4") {
     labelNome.setAttribute('style', 'color: red')
     labelNome.innerHTML = ' *Digite seu nome completo!'
     nome.setAttribute('style', 'border-color: red')
@@ -41,47 +35,35 @@ nome.addEventListener('keyup', () => {
     validNome = true
   }
 })
-
 //Campo Email
 email.addEventListener('keyup', () => {
-  if(email.value.length <= "2" || email.value.length == ""){
+
+  if(validateEmail(email.value) != true){
     labelEmail.setAttribute('style', 'color: red')
     labelEmail.innerHTML = ' *Digite seu email completo!'
     email.setAttribute('style', 'border-color: red')
     validEmail = false
-  } else {
+  }else{
     labelEmail.setAttribute('style', 'color: #14abec')
     labelEmail.innerHTML = 'Email'
     email.setAttribute('style', 'border-color: #14abec')
     validEmail = true
   }
-})
-
-//Campo confirme email
-
-confirmEmail.addEventListener('keyup', () => {
-  if(email.value != confirmEmail.value){
-    labelConfirmEmail.setAttribute('style', 'color: red')
-    labelConfirmEmail.innerHTML = 'Confirmar Email *Os email não conferem.'
-    confirmEmail.setAttribute('style', 'border-color: red')
-    validConfirmEmail = false
-  } else {
-    labelConfirmEmail.setAttribute('style', 'color: #14abec')
-    labelConfirmEmail.innerHTML = 'Confirmar Email'
-    confirmEmail.setAttribute('style', 'border-color: #14abec')
-    validConfirmEmail = true
-  }
+ 
 })
 
 
 //campo CPF
 cpf.addEventListener('keyup', () => {
-   if(isNaN(cpf.value.length = cpf.substring(0, cpf.length - 1) )){
-     labelCpf.setAttribute('style', 'color: red')
+  
+  if (cpf.value.length < 14 ) {
+    
+    labelCpf.setAttribute('style', 'color: red')
     labelCpf.innerHTML = ' *Digite seu CPF corretamente!'
     cpf.setAttribute('style', 'border-color: red')
     validCpf = false
   } else {
+    
     labelCpf.setAttribute('style', 'color: #14abec')
     labelCpf.innerHTML = 'CPF'
     cpf.setAttribute('style', 'border-color: #14abec')
@@ -106,16 +88,21 @@ function campoCpf(cpf) {
 
 // Campo telefone 
 telefone.addEventListener('keyup', () => {
-  if(telefone.value.length <= "2" || telefone.value.length == ""){
+  
+  if (telefone.value.length < "15") {
+    
     labelTelefone.setAttribute('style', 'color: red')
     labelTelefone.innerHTML = ' *Digite seu numero corretamente!'
     telefone.setAttribute('style', 'border-color: red')
     validTelefone = false
+
   } else {
+    validTelefone = true
     labelTelefone.setAttribute('style', 'color: #14abec')
     labelTelefone.innerHTML = 'telefone'
     telefone.setAttribute('style', 'border-color: #14abec')
-    validTelefone = true
+    
+    
   }
 })
 
@@ -126,69 +113,51 @@ function mtel(e) {
   v = v.replace(/\D/g, ""); //Remove tudo o que não é dígito
   v = v.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
   v = v.replace(/(\d)(\d{4})$/, "$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
-  
+
   //console.log(v);
   return e.target.value = v;
 }
 
-
 //criar storage
+function cadastrar() {
+  if (validNome == true  && validEmail == true && validCpf == true && validTelefone == true) {
+  
+  // let listaUserItem = "listaUser"+Math.floor(Math.random() * 11);
 
-function cadastrar(){
-  if(validNome && validCpf && validEmail && validConfirmEmail && validTelefone){
-    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
-    
+   let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
     listaUser.push(
-    {
-      nomeCad: nome.value,
-      cpfCad: cpf.value,
-      emailCad: email.value,
-      telefoneCad: telefone.value,
-    }
+      {
+        nomeCad: nome.value,
+        emailCad: email.value,
+        cpfCad: cpf.value,
+        telefoneCad: telefone.value
+      }
     )
-    
-    localStorage.setItem('listaUser', JSON.stringify(listaUser))
-    
    
+    localStorage.setItem('listaUser', JSON.stringify(listaUser))
+
     msgSuccess.setAttribute('style', 'display: block')
     msgSuccess.innerHTML = '<strong>Criando seu cadastro...</strong>'
     msgError.setAttribute('style', 'display: none')
     msgError.innerHTML = ''
-    
-    setTimeout(()=>{
-        window.location.href = "file:///home/dog/Documents/frontend-bancopan/formulario/editar.html"
+      
+    setTimeout(() => {
+      window.location.href = "editar.html"
     }, 3000)
   
-    
-  } else {
+  }else {
     msgError.setAttribute('style', 'display: block')
     msgError.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
     msgSuccess.innerHTML = ''
     msgSuccess.setAttribute('style', 'display: none')
+        
+    } 
   }
-}
 
-btn.addEventListener('click', ()=>{
-  let inputEmail = document.querySelector('#email')
   
-  if(inputEmail.getAttribute('type') == 'text'){
-    inputEmail.setAttribute('type', 'text')
-  } else {
-    inputEmail.setAttribute('type', 'text')
+  function validateEmail(email) {
+    var email = /\S+@\S+\.\S+/;
+    return email.test(email);
   }
-})
 
-btnConfirm.addEventListener('click', ()=>{
-  let inputConfirmEmail = document.querySelector('#confirmEmail')
   
-  if(inputEmail.getAttribute('type') == 'text'){
-    inputEmail.setAttribute('type', 'text')
-  } else {
-    inputConfirmEmail.setAttribute('type', 'text')
-  }
-})
-
-
-
-
-
--- endpoint.js
+++ endpoint.js
@@ -1,44 +1,150 @@
-//ENDPOINT - GET
 
-function fazGet(url){
+//ENDPOINT - GET
+/*function fazGet(url){
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
     request.send()
     return request.responseText
   }
-  
   function criaLinha(usuario){
-    linha = document.createElement("tr");
+    /*linha = document.createElement("tr");
     tdNome = document.createElement("tr");
     tdEmail = document.createElement("tr");
+    tdConfEmail = document.createElement("tr");
     tdCpf = document.createElement("tr");
     tdPhone = document.createElement("tr");
-    
-      
+       
     tdNome.innerHTML = usuario.name
     tdEmail.innerHTML = usuario.email
+    tdConfEmail.innerHTML = usuario.confirmEmail
     tdCpf.innerHTML = usuario.cpf
     tdPhone.innerHTML = usuario.phone
   
     linha.appendChild(tdNome);
     linha.appendChild(tdEmail);
+    linha.appendChild(tdConfEmail);
     linha.appendChild(tdCpf);
-    linha.appendChild(tdPhone);
-    
-    return linha;
-  }
+    linha.appendChild(tdPhone);*/
+/*   
+ document.getElementById('nome').value = usuario.name;
+ document.getElementById('email').value = usuario.email;
+ document.getElementById('cpf').value = usuario.cpf;
+ document.getElementById('telefone').value = usuario.telefone;
+	
+   //return linha;
+ }
+ function main(){
+  let data = fazGet("https://private-56d1e-charlesaraujodasilva.apiary-mock.com/users")
+   let usuarios = JSON.parse(data);
+     let formulario = document.getElementById("formulario")
+       usuarios.forEach(element => {
+       let linha = criaLinha(element);
+       formulario.appendChild(linha);
+     });
+   //Para cada usuario
+      //criar uma linha
+      //adicionar na formulario
+ }
+ 
+ main()*/
+/*
+let listaUser = localStorage.getItem('listaUser');
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Nome";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Email";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "CPF";
+let heading_4 = document.createElement('th');
+heading_4.innerHTML = "Telefone";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+row_1.appendChild(heading_4);
+thead.appendChild(row_1);
+
+var obj = JSON.parse(listaUser);
+
+var a = obj;
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+for (let i in a) {
+
+  let row_2 = document.createElement('tr');
+  let row_2_data_1 = document.createElement('td');
+  row_2_data_1.innerHTML = a[i]['nomeCad'];
+  let row_2_data_2 = document.createElement('td');
+  row_2_data_2.innerHTML = a[i]['emailCad'];
+  let row_2_data_3 = document.createElement('td');
+  row_2_data_3.innerHTML = a[i]['cpfCad'];
+  let row_2_data_4 = document.createElement('td');
+  row_2_data_4.innerHTML = a[i]['telefoneCad'];
   
-  function main(){
-   let data = fazGet("https://private-56d1e-charlesaraujodasilva.apiary-mock.com/users")
-    let usuarios = JSON.parse(data);
-    let formulario = document.getElementById("formulario")
-    usuarios.forEach(element => {
-      let linha = criaLinha(element);
-      formulario.appendChild(linha);
-    });
-    //Para cada usuario
-       //criar uma linha
-       //adicionar na formulario
-  }
+  row_2.appendChild(row_2_data_1);
+  row_2.appendChild(row_2_data_2);
+  row_2.appendChild(row_2_data_3);
+  row_2.appendChild(row_2_data_4);
+  tbody.appendChild(row_2);
+}*/
+
+let listaUser = localStorage.getItem('listaUser');
+var obj = JSON.parse(listaUser);
+
+var a = obj;
+table = "";
+
+for (let i in a) {
   
-  main()
+  table += " <div class='label-float'>" +
+  "<input type='text' id='nome' value="+a[i]['nomeCad']+">" +
+  "<label id='labelNome' for='nome'>Nome</label>" +
+  "</div>";
+
+  table += " <div class='label-float'>" +
+  "<input type='text' id='email' value="+a[i]['emailCad']+">" +
+  "<label id='labelNome' for='email'>Email</label>" +
+  "</div>";
+
+  table += " <div class='label-float'>" +
+  "<input type='text' id='cpf'  value="+a[i]['cpfCad']+">" +
+  "<label id='labelNome' for='cpf'>CPF</label>" +
+  "</div>";
+
+  table += " <div class='label-float'>" +
+  "<input type='text' id='telefone'  value="+a[i]['telefoneCad']+">" +
+  "<label id='labelNome' for='telefone'>Telefone</label>" +
+  "</div>";
+
+  table += "<div class='justify-center'>" +
+  "<button onclick='editar("+a[i]['cpfCad']+")'>Editar</button>" +
+  "<button type='button' onclick=excluir('"+a[i]['cpfCad']+"')>Excluir </button>" +
+  "</div>";
+}
+
+document.getElementById('sovai').innerHTML = table;
+
+
+function excluir(cpf){
+alert(cpf);
+let listaUser = localStorage.getItem('listaUser');
+var obj = JSON.parse(listaUser);
+
+var a = obj;
+
+
+for (let i in a) {
+    if(cpf === a[i]['cpfCad'] ){
+      localStorage.removeItem('cpfCad')
+    }
+}
+}
+
+
